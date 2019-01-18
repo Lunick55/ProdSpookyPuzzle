9385b1ac5243132c8be3005d92cce42db965a37e
Lunick <karimelzanaty98@gmail.com>
2019-01-18 01:55:52 -0500

    First commit. Added basic movement structure.

---
+++ SpoopyPuzzle/Assets/Code/Managers/InputManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    //private static InputManager inputManager;
+
+    //public static InputManager instance
+    //{
+    //    get
+    //    {
+    //        if (!inputManager)
+    //        {
+    //            inputManager = FindObjectOfType(typeof(InputManager)) as InputManager;
+
+    //            if (!inputManager)
+    //            {
+    //                Debug.LogError("There needs to be one active InputManager script on a GameObject in your scene.");
+    //            }
+    //            else
+    //            {
+    //                inputManager.Init();
+    //            }
+    //        }
+
+    //        return inputManager;
+    //    }
+    //}
+
+    void Init()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            EventManager.FireEvent("UP");
+            Debug.Log("UP");
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            EventManager.FireEvent("LEFT");
+            Debug.Log("LEFT");
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            EventManager.FireEvent("DOWN");
+            Debug.Log("DOWN");
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            EventManager.FireEvent("RIGHT");
+            Debug.Log("RIGHT");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            EventManager.FireEvent("UPup");
+            Debug.Log("UPup");
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            EventManager.FireEvent("LEFTup");
+            Debug.Log("LEFTup");
+        }
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            EventManager.FireEvent("DOWNup");
+            Debug.Log("DOWNup");
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            EventManager.FireEvent("RIGHTup");
+            Debug.Log("RIGHTup");
+        }
+    }
+}
---
+++ SpoopyPuzzle/Assets/Code/Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float gravity = 0.0f;
+    [SerializeField] float jumpSpeed = 0.0f;
+    float vSpeed = 0;
+
+    CharacterController cc;
+
+    private bool canHandleInput = true;
+    private Vector3 inputDir = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.StartListening("UP", HandleUp);
+        EventManager.StartListening("DOWN", HandleDown);
+        EventManager.StartListening("LEFT", HandleLeft);
+        EventManager.StartListening("RIGHT", HandleRight);
+
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        if (canHandleInput)
+        {
+            //sets the left and right movement
+            inputDir = new Vector3(inputDir.x, 0.0f, inputDir.z);
+
+            //apply speed
+            inputDir = inputDir * speed;
+
+            //check if grounded
+            if (cc.isGrounded)
+            {
+                Debug.Log("I'm on the ground");
+                vSpeed = 0;
+
+                //TEMP::jump thing. 
+                //TODO: move to input manager
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    vSpeed = jumpSpeed;
+                }
+            }
+
+
+            //apply accelerating gravity
+            inputDir.y = (vSpeed -= (gravity * Time.deltaTime)); 
+
+            //move the object
+            cc.Move(inputDir * Time.deltaTime); // this might need to get out of the if statement
+            Debug.Log(inputDir.y);
+        }
+
+        inputDir = Vector3.zero;
+    }
+
+    //sets directions based on input handler
+    void HandleLeft()
+    {
+        inputDir.x = -1;
+    }
+    void HandleRight()
+    {
+        inputDir.x = 1;
+    }
+    void HandleUp()
+    {
+        inputDir.z = 1;
+    }
+    void HandleDown()
+    {
+        inputDir.z = -1;
+    }
+}
